05734631441691b60a4b38fa7355fbbc4863bb9c
Alessandro <alessandro.demarche00@gmail.com>
2021-01-27 13:36:22 -0300

    add icon/error msg

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let weather = {
        fetch("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&lang=pt_br&units=metric&appid="+ this.apiKey)
        .then((response) => response.json())
        .then((data) => this.displayWeather(data))
+       .catch((err) => alert("Não conseguimos acessar sua busca. Tente novamente"))
    },
    
    displayWeather: function(data) {
@@ -13,8 +14,6 @@ let weather = {
        const { temp, humidity } = data.main
        const { description, icon, main} = data.weather[0]
        const { speed } = data.wind 
-       console.log(name,temp,humidity,description,icon,speed);
-       console.log(typeof description);
        document.querySelector(".city").innerHTML = "Tempo em " + name
        document.querySelector(".temp").innerHTML = Math.floor(temp) + "°C"
        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
@@ -30,25 +29,25 @@ let weather = {
 
    displayBackground: function(description){
        if(description === "céu limpo"){
-            document.body.style.backgroundImage = "url(./assets/clearsky.jpg)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/clearsky.jpg)"
        }
        if(description === "algumas nuvens" || description === "nuvens dispersas"){
-            document.body.style.backgroundImage = "url(./assets/fewclouds.jpg)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/fewclouds.jpg)"
        }
        if(description === "nublado"){
-            document.body.style.backgroundImage = "url(./assets/nublado.jpg)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/nublado.jpg)"
        }
        if(description === "chuva moderada" || description === "chuva leve" || description === "chuva forte"){
-            document.body.style.backgroundImage = "url(./assets/rains.jpg)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/rains.jpg)"
        }
        if(description === "tempestade"){
-            document.body.style.backgroundImage = "url(./assests/thenderstorm.jpg)"
+            document.body.style.backgroundImage = "url(./assests/backgrounds/thenderstorm.jpg)"
        }
        if(description === "neve" || description === "pouca neve"){
-            document.body.style.backgroundImage = "url(./assets/snow)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/snow)"
        }
        if(description === "neblina"){
-            document.body.style.backgroundImage = "url(./assets/mist)"
+            document.body.style.backgroundImage = "url(./assets/backgrounds/mist)"
        }
    }
 }
